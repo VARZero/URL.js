8a7601bb6f76821c2d47a9e0f75a9f12a54fd303
sh1nj1h00n <shinjh0158@gmail.com>
2022-09-03 15:45:37 +0900

    express 구현

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var express = require('express');
 var app = express();
 var router = require('./router/main')(app);
 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set('views', __dirname + '/view'); //html 주소
+app.set('view engine', 'ejs'); 
 app.engine('html', require('ejs').renderFile);
 
 var server = app.listen(3000, function(){
