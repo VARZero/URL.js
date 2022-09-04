5c3c1fdf1ea70f28e04537771468a9ff3114e4e5
sh1nj1h00n <shinjh0158@gmail.com>
2022-09-04 16:24:33 +0900

    랜덤 코드

--- router/main.js
+++ router/main.js
@@ -1,5 +1,10 @@
 module.exports = function(app)
 {
+   app.get('
+   let randomStr = Math.random().toString(36).substring(2, 12);
+   console.log(randomStr);' , function(req,res))   
+
+
      app.get('/',function(req,res){
         res.render('index.html')
      });
